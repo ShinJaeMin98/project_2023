ebd7a2abf0e5730676c6363788de9f8d947624d2
ShinJaeMin98 <jaemin6292@gmail.com>
2024-01-08 14:30:40 +0900

    파일 공통 기능 구현

--- src/main/java/org/choongang/member/controllers/RequestJoin.java
+++ src/main/java/org/choongang/member/controllers/RequestJoin.java
@@ -6,17 +6,22 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 public class RequestJoin {
+
+    private String gid = UUID.randomUUID().toString();
+
     @NotBlank @Email
     private String email;
 
     @NotBlank
-    @Size(min = 6)
+    @Size(min=6)
     private String userId;
 
     @NotBlank
-    @Size(min = 8)
+    @Size(min=8)
     private String password;
 
     @NotBlank
@@ -27,6 +32,5 @@ public class RequestJoin {
 
     @AssertTrue
     private boolean agree;
-
 
 }
